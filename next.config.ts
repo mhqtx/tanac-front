a5308ddac7d2c0466c4535ec94800895ff005c2b
Milan Hakaj <milan.hakaj@quantox.com>
2025-08-28 21:21:11 +0200

    next conf fix

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "api.mhqtx.xyz/tanac",
+        hostname: "api.mhqtx.xyz",
         pathname: "/tanac/wp-content/uploads/**",
       },
     ],
