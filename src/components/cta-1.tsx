5bf230d346fdf86a464051f9f6657152774b5dd8
Milan Hakaj <milan.hakaj@quantox.com>
2025-08-30 21:44:48 +0200

    fixxx

--- src/components/cta-1.tsx
+++ src/components/cta-1.tsx
@@ -9,7 +9,7 @@ interface Props {
 export function Cta1({ title, description, children }: Props) {
   return (
     <section id="cta" className="w-full bg-green-200 px-2 py-10">
-      <div className="animation-reveal container text-center lg:w-1/2 mx-auto">
+      <div className="animation-reveal container text-center lg:w-[60%] mx-auto">
         <h2 className="mt-2 space-x-1 text-3xl font-bold">{title}</h2>
         <p className="mb-4 mt-3 text-lg md:text-xl">{description}</p>
         <div>{children}</div>
