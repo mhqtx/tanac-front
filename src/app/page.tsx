acf37f6cb5822680151e0b7611e94a23d02a633e
Milan Hakaj <milan.hakaj@quantox.com>
2025-08-30 21:07:55 +0200

    servicces1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import type { WP_REST_API_Posts, WP_REST_API_Page } from "wp-types";
 import { Hero2 } from "@/components/hero-2";
 import { Nav1 } from "@/components/nav-1";
+import { Services1 } from "@/components/services-1";
 
 async function safeFetch<T>(url: string): Promise<T | null> {
   try {
@@ -103,52 +104,12 @@ export default async function Home() {
         </div>
       </section>
 
-      <div
-        id="services"
-        className="flex bg-foreground-primary px-2 py-10 lg:py-10"
-      >
-        <div className="animation-reveal container mx-auto">
-          <div className="xl:flex xl:space-x-10">
-            <div className="mb-10 xl:w-[35%]">
-              {/* TODO: Add photo here? */}
-              <h2 className="text-3xl font-bold">
-                {page?.acf?.services_title ?? "..."}
-              </h2>
-              <p className="mb-4 mt-3 text-lg md:text-xl">
-                {page?.acf?.services_description ?? "..."}
-              </p>
-              <div>
-                <button className="flex items-center justify-center capitalize font-bold h-5 bg-black text-white rounded-full px-6 py-2 text-sm transition-all duration-200 hover:bg-neutral-800 focus:outline-none focus:ring-2 focus:ring-white/50 active:bg-neutral-900">
-                  Message Us
-                </button>
-              </div>
-            </div>
-            <div className="flex flex-wrap xl:w-[65%]">
-              {services?.length ? (
-                services.map((item, index) => (
-                  <div
-                    key={index}
-                    className="mb-4 flex w-full flex-col rounded-xl pr-2 last:mb-0 md:w-1/2 lg:w-1/2"
-                  >
-                    <div className="flex items-center space-x-1">
-                      <span className="flex h-5 w-5 shrink-0 items-center justify-center rounded-full bg-black font-bold text-white">
-                        {index + 1}
-                      </span>
-                      <h3 className="text-lg font-bold">
-                        {item.title?.rendered ?? "Untitled Service"}
-                      </h3>
-                    </div>
-                    <div className="my-2 h-[1px] w-full bg-gradient-to-r from-primary" />
-                    <p>{item.content?.rendered ?? ""}</p>
-                  </div>
-                ))
-              ) : (
-                <p>No services available.</p>
-              )}
-            </div>
-          </div>
-        </div>
-      </div>
+      <Services1
+        title={page?.acf?.services_title ?? "..."}
+        description={page?.acf?.services_description ?? "..."}
+        services={services ?? []}
+      />
+
       <section
         id="contact"
         className="animation-reveal flex flex-col _bg-white lg:flex-row w-full"
