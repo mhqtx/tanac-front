9ce89be089bcc33a3c137fee4f8a6ee340919057
Milan Hakaj <milan.hakaj@quantox.com>
2025-08-31 13:08:03 +0200

    fixxx

--- src/components/contact-1.tsx
+++ src/components/contact-1.tsx
@@ -63,7 +63,7 @@ export function Contact1({
                     d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
                   />
                 </svg>
-                <span>Location</span>
+                <span>Adresa</span>
               </h6>
               <h6 className="text-md mb-1 flex items-start">
                 <div className="flex flex-col">{address}</div>
@@ -86,7 +86,7 @@ export function Contact1({
                   d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
                 />
               </svg>
-              <span>Working hours</span>
+              <span>Radno vreme</span>
             </h6>
             <div
               dangerouslySetInnerHTML={{
@@ -134,7 +134,7 @@ export function Contact1({
                   d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
                 />
               </svg>
-              <span>Contact</span>
+              <span>Kontakt</span>
             </h6>
 
             <h6 className="text-md flex items-start">
